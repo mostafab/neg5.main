7e859c426bd8c488688f02333169be64c1a8b881
mostafab <mostafa0104@gmail.com>
2015-11-30 16:49:16 -0500

    new aws

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
         "clientID" : "722139957351-ri5voimk85qmt70cq3n42nh1i5724fm1.apps.googleusercontent.com",
         "clientSecret" : "jw-R4MCmemPxK-L2nxJNbPmF",
         "callbackURLLocal" : "http://localhost:1337/auth/google/callback",
-        "callbackURL" : "http://ec2-54-152-3-151.compute-1.amazonaws.com:1337/auth/google/callback"
+        "callbackURL" : "http://ec2-54-88-204-155.compute-1.amazonaws.com:1337/auth/google/callback"
     }
 };
