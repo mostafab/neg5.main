02ea263b1673ba5952750d4271e7ab0302c39e69
mostafab <mostafa0104@gmail.com>
2016-06-15 22:59:09 -0400

    finalize sql table statements

--- docs/database/create_tables.sql
+++ docs/database/create_tables.sql
@@ -1,12 +1,12 @@
-CREATE TABLE user (
+CREATE TABLE account (
     username varchar(20) NOT NULL,
     hash varchar(255) NOT NULL,
     hidden boolean DEFAULT false,
-    primary key (username)
+    PRIMARY KEY (username) 
 );
 
 CREATE TABLE tournament (
-    id serial NOT NULL,
+    id varchar(20) NOT NULL,
     name varchar(255) NOT NULL,
     tournament_date date,
     question_set varchar(255),
@@ -14,23 +14,23 @@ CREATE TABLE tournament (
     hidden boolean,
     director_id varchar(20) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (director_id) REFERENCES user(username)
+    FOREIGN KEY (director_id) REFERENCES account(username) ON DELETE SET NULL
 );
 
 CREATE TABLE tournament_team (
-    id serial NOT NULL,
-    name varchar(50) NOT NULL,
-    tournament_id integer NOT NULL,
+    id varchar(20) NOT NULL,
+    name varchar(50) NOT NULL CHECK (char_length(name) > 0),
+    tournament_id varchar(20) NOT NULL,
     added_by varchar(20) NOT NULL,
     PRIMARY KEY (id, tournament_id),
-    FOREIGN KEY (tournament_id) REFERENCES tournament(id),
-    FOREIGN KEY (added_by) REFERENCES user,
+    FOREIGN KEY (tournament_id) REFERENCES tournament(id) ON DELETE CASCADE,
+    FOREIGN KEY (added_by) REFERENCES account(username) ON DELETE SET NULL,
     UNIQUE (name, tournament_id)
 );
 
 CREATE TABLE tournament_match (
-    id serial NOT NULL,
-    tournament_id integer NOT NULL,
+    id varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
     round integer DEFAULT 0 CHECK (round >= 0),
     room VARCHAR(20),
     moderator VARCHAR(20),
@@ -38,6 +38,109 @@ CREATE TABLE tournament_match (
     tossups_heard integer DEFAULT 0 CHECK (tossups_heard >= 0),
     added_by varchar(20) NOT NULL,
     PRIMARY KEY (id, tournament_id),
-    FOREIGN KEY (tournament_id) REFERENCES tournament(id)
+    FOREIGN KEY (tournament_id) REFERENCES tournament(id) ON DELETE CASCADE,
+    FOREIGN KEY (added_by) REFERENCES account(username) ON DELETE SET NULL
 );
+
+CREATE TABLE tournament_player (
+    id varchar(20) NOT NULL,
+    name varchar(50) NOT NULL CHECK (char_length(name) > 0),
+    team_id varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    added_by varchar(20) NOT NULL,
+    PRIMARY KEY (id, tournament_id),
+    FOREIGN KEY (team_id, tournament_id) REFERENCES tournament_team(id, tournament_id) ON DELETE CASCADE,
+    FOREIGN KEY (added_by) REFERENCES account(username) ON DELETE SET NULL
+);
+
+CREATE TABLE user_collaborates_on_tournament (
+    username varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    is_admin boolean DEFAULT false,
+    PRIMARY KEY (username, tournament_id),
+    FOREIGN KEY (username) REFERENCES account(username) ON DELETE CASCADE,
+    FOREIGN KEY (tournament_id) REFERENCES tournament(id) ON DELETE CASCADE
+);
+
+CREATE TYPE answer_type AS ENUM('Power', 'Base', 'Neg');
+
+CREATE TABLE tournament_tossup_values (
+    tournament_id varchar(20) NOT NULL,
+    tossup_value integer NOT NULL,
+    tossup_answer_type answer_type NOT NULL,
+    PRIMARY KEY (tournament_id, tossup_value),
+    FOREIGN KEY (tournament_id) REFERENCES tournament(id) ON DELETE CASCADE
+);
+
+CREATE TABLE player_plays_in_tournament_match (
+    player_id varchar(20) NOT NULL,
+    match_id varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    tossups_heard integer DEFAULT 0 CHECK (tossups_heard >= 0),
+    PRIMARY KEY (player_id, match_id, tournament_id),
+    FOREIGN KEY (player_id, tournament_id) REFERENCES tournament_player(id, tournament_id) ON DELETE CASCADE,
+    FOREIGN KEY (match_id, tournament_id) REFERENCES tournament_match(id, tournament_id) ON DELETE CASCADE   
+);
+
+CREATE TABLE team_plays_in_tournament_match (
+    team_id varchar(20) NOT NULL,
+    match_id varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    score integer NOT NULL,
+    bounceback_points integer DEFAULT 0 CHECK (bounceback_points >= 0),
+    PRIMARY KEY (team_id, match_id, tournament_id),
+    FOREIGN KEY (team_id, tournament_id) REFERENCES tournament_team(id, tournament_id) ON DELETE CASCADE,
+    FOREIGN KEY (match_id, tournament_id) REFERENCES tournament_match(id, tournament_id) ON DELETE CASCADE  
+);
+
+CREATE TABLE player_match_tossup (
+    player_id varchar(20) NOT NULL,
+    match_id varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    tossup_value integer NOT NULL,
+    number_gotten integer DEFAULT 0 NOT NULL CHECK (number_gotten >= 0),
+    PRIMARY KEY (player_id, match_id, tournament_id, tossup_value),
+    FOREIGN KEY (player_id, tournament_id) REFERENCES tournament_player(id, tournament_id) ON DELETE CASCADE,
+    FOREIGN KEY (match_id, tournament_id) REFERENCES tournament_match(id, tournament_id) ON DELETE CASCADE,
+    FOREIGN KEY (tournament_id, tossup_value) REFERENCES tournament_tossup_values(tournament_id, tossup_value) ON DELETE NO ACTION
+);
+
+CREATE TABLE tournament_phase (
+    id varchar(20) NOT NULL,
+    name varchar(50) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    PRIMARY KEY (id, tournament_id),
+    FOREIGN KEY (tournament_id) REFERENCES tournament(id) ON DELETE CASCADE  
+);
+
+CREATE TABLE match_is_part_of_phase (
+    match_id varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    phase_id varchar(20) NOT NULL,
+    PRIMARY KEY (match_id, tournament_id, phase_id),
+    FOREIGN KEY (match_id, tournament_id) REFERENCES tournament_match(id, tournament_id) ON DELETE CASCADE,
+    FOREIGN KEY (phase_id, tournament_id) REFERENCES tournament_phase(id, tournament_id)
+);
+
+CREATE TABLE tournament_division (
+    id varchar(20) NOT NULL,
+    name varchar(50) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    phase_id varchar(20) NOT NULL,
+    PRIMARY KEY (id, tournament_id),
+    FOREIGN KEY (phase_id, tournament_id) REFERENCES tournament_phase(id, tournament_id) ON DELETE CASCADE,
+    UNIQUE (tournament_id, phase_id, name)
+);
+
+CREATE TABLE tournament_team_in_division (
+    team_id varchar(20) NOT NULL,
+    division_id varchar(20) NOT NULL,
+    tournament_id varchar(20) NOT NULL,
+    PRIMARY KEY (team_id, division_id, tournament_id),
+    FOREIGN KEY (division_id, tournament_id) REFERENCES tournament_division(id, tournament_id) ON DELETE CASCADE,
+    FOREIGN KEY (team_id, tournament_id) REFERENCES tournament_team(id, tournament_id) ON DELETE CASCADE
+);
+
+
+
 
