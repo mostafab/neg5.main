e28d7f313e3e543b3f2af84f12032ca310e0f49b
mostafab <mostafa0104@gmail.com>
2016-07-12 17:48:49 -0400

    modify findMatchesByTournament query to return phases as a two dimensional array

--- app/sql/match-sql/findMatchesByTournament.sql
+++ app/sql/match-sql/findMatchesByTournament.sql
@@ -1,11 +1,12 @@
 
-SELECT  match_phases.match_id, outer_team_result.tournament_id, 
+SELECT  match_info.match_id, outer_team_result.tournament_id, 
         outer_team_result.team_1_id, outer_team_result.team_1_name, outer_team_result.team_1_score,
         outer_team_result.team_2_id, outer_team_result.team_2_name, outer_team_result.team_2_score,
          
-        match_phases.round, match_phases.room, match_phases.moderator,
-        match_phases.packet, match_phases.tossups_heard, match_phases.added_by,
-        match_phases.phase_ids, match_phases.phase_names    
+        match_info.round, match_info.room, match_info.moderator,
+        match_info.packet, match_info.tossups_heard, match_info.added_by,
+        -- match_info.phase_ids, match_info.phase_names   
+        match_info.phases 
 
 FROM
 (
@@ -36,7 +37,7 @@ FROM
                             team_plays_in_tournament_match team_1_match
                             ON team_1.id = team_1_match.team_id AND team_1.tournament_id = team_1_match.tournament_id
                             
-                            WHERE team_1.tournament_id = $1
+                            -- WHERE team_1.tournament_id = $1
 
                         ) AS team_1_info
 
@@ -50,7 +51,7 @@ FROM
                             team_plays_in_tournament_match team_2_match
                             ON team_2.id = team_2_match.team_id AND team_2.tournament_id = team_2_match.tournament_id
 
-                            WHERE team_2.tournament_id = $1
+                            -- WHERE team_2.tournament_id = $1
 
                         ) AS team_2_info
 
@@ -68,20 +69,21 @@ FROM
     -- Get all matches and phases that they are a part of
     (
         SELECT M.tournament_id as tid, M.id AS match_id, M.round, M.room, M.moderator, M.packet, M.tossups_heard, M.added_by, 
-                array_agg(P.id) AS phase_ids, array_agg(P.name) AS phase_names
+                array_agg(array[P.id::varchar, P.name::varchar]) AS phases
+                
         FROM  
         match_is_part_of_phase MP, tournament_phase P, tournament_match M
         WHERE MP.phase_id = P.id AND MP.tournament_id = P.tournament_id AND MP.tournament_id = '1'
                 AND M.tournament_id = MP.tournament_id AND M.id = MP.match_id
-                AND M.tournament_id = $1
+                -- AND M.tournament_id = $1
                 
         GROUP BY M.id, M.tournament_id
 
         ORDER BY M.round 
 
-    ) AS match_phases
+    ) AS match_info
 
-    ON outer_team_result.tournament_id = match_phases.tid AND outer_team_result.match_id = match_phases.match_id
+    ON outer_team_result.tournament_id = match_info.tid AND outer_team_result.match_id = match_info.match_id
 
 )
 
--- docs/database/sql_statements/create_indexes.sql
+++ docs/database/sql_statements/create_indexes.sql
@@ -1,11 +1,25 @@
-CREATE INDEX hash_on_director_id ON tournament USING hash (director_id);
+CREATE INDEX hash_on_director_id ON tournament (director_id);
 
-CREATE INDEX hash_on_tournament_team_tournament_id ON tournament_team USING hash (tournament_id);
+CREATE INDEX hash_on_tournament_team_tournament_id ON tournament_team (tournament_id);
 
-CREATE INDEX hash_on_tournament_match_tournament_id ON tournament_match USING hash (tournament_id);
+CREATE INDEX hash_on_tournament_match_tournament_id ON tournament_match (tournament_id);
+CREATE INDEX index_on_tournament_match_round ON tournament_match (round);
 
-CREATE INDEX hash_on_tournament_player_tournament_id ON tournament_playerUSING hash (tournament_id);
+CREATE INDEX hash_on_tournament_player_tournament_id ON tournament_player (tournament_id);
 
-CREATE INDEX hash_on_user_collaborates_username ON tournament_team USING hash (username);
+CREATE INDEX hash_on_user_collaborates_username ON tournament_team (username);
 
-CREATE INDEX hash_on_tournament_tossup_values ON tournament_tossup_values USING hash (tournament_id);
+CREATE INDEX hash_on_tournament_tossup_values_tournament_id ON tournament_tossup_values (tournament_id);
+
+CREATE INDEX hash_on_player_plays_in_match_tournament_id ON player_plays_in_tournament_match (tournament_id);
+CREATE INDEX hash_on_player_plays_in_match_player_id ON player_plays_in_tournament_match (player_id);
+
+CREATE INDEX hash_on_team_plays_in_match_tournament_id ON team_plays_in_tournament_match (tournament_id);
+CREATE INDEX hash_on_team_plays_in_match_team_id ON team_plays_in_tournament_match (team_id);
+
+CREATE INDEX hash_on_player_match_tossup_tournament_id ON player_match_tossup (tournament_id);
+CREATE INDEX hash_on_player_match_tossup_player_id ON player_match_tossup (player_id);
+CREATE INDEX hash_on_player_match_tossup_match_id ON player_match_tossup (match_id);
+
+CREATE INDEX hash_on_tournament_phase_tournament_id ON tournament_phase (tournament_id);
+
