007208f6c5eb79869da1f8c8f67272020d24e8d6
mostafab <mostafa0104@gmail.com>
2016-07-13 21:33:56 -0400

    add addTeamToTournament sql query

--- app/sql/team-sql/findTeamsByTournament.sql
+++ app/sql/team-sql/findTeamsByTournament.sql
@@ -20,7 +20,7 @@ FROM
                 INNER JOIN
                 tournament_division division
                 ON team_division.tournament_id = division.tournament_id AND team_division.division_id = division.id 
-                -- WHERE division.tournament_id = $1
+                WHERE division.tournament_id = $1
             ) AS team_divisions
 
             INNER JOIN
@@ -28,7 +28,7 @@ FROM
             tournament_phase phase 
 
             ON phase.id = team_divisions.phase_id AND phase.tournament_id = team_divisions.tournament_id 
-
+            
             GROUP BY team_divisions.team_id, phase.tournament_id
 
         ) AS test
