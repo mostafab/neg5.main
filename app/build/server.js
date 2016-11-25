6e22896110515f57fc42c1e442b49ff9e862c917
mostafa <mbhuiyan8@gatech.edu>
2016-11-25 15:06:02 -0500

    both http and https

--- app/build/server.js
+++ app/build/server.js
@@ -41,11 +41,10 @@ var startServer = function startServer() {
             cert: _fs2.default.readFileSync(httpsDir + certName, 'utf8')
         };
         _https2.default.createServer(options, app).listen(httpsPort);
-        console.log('Https server running on port ' + httpsPort);
-    } else {
-        _http2.default.createServer(app).listen(PORT_NUM);
-        console.log('Express server running on port ' + PORT_NUM);
+        console.log('Express https server running on port ' + httpsPort);
     }
+    _http2.default.createServer(app).listen(PORT_NUM);
+    console.log('Express http server running on port ' + PORT_NUM);
 };
 
 startServer();
--- app/src/server.js
+++ app/src/server.js
@@ -17,11 +17,10 @@ const startServer = () => {
             cert: fs.readFileSync(httpsDir + certName, 'utf8')
         }
         https.createServer(options, app).listen(httpsPort);
-        console.log('Https server running on port ' + httpsPort);
-    } else {
-        http.createServer(app).listen(PORT_NUM);
-        console.log('Express server running on port ' + PORT_NUM);
+        console.log('Express https server running on port ' + httpsPort);
     }
+    http.createServer(app).listen(PORT_NUM);
+    console.log('Express http server running on port ' + PORT_NUM);
 }
 
 startServer();
