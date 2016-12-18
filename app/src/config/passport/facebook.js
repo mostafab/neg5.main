8d562d96fffdfb7fc950709f7485425e9245446e
mostafa <mbhuiyan8@gatech.edu>
2016-12-18 13:26:27 -0500

    safer startup for passport-config

--- app/src/config/passport/facebook.js
+++ app/src/config/passport/facebook.js
@@ -3,19 +3,23 @@ import Config from './config';
 
 export default (passport) => {
     
-    const {clientID, clientSecret, callbackURL} = Config.facebook;
+    if (Config.facebook) {
+        
+        const {clientID, clientSecret, callbackURL} = Config.facebook;
     
-    passport.use(new FacebookStrategy(
-        {
-            clientID,
-            clientSecret,
-            callbackURL            
-        },
-        (accessToken, refreshToken, profile, verifyCallback) => {
-            const user = profile;
-            user.token = 'TEST TOKEN';
-            verifyCallback(null, user);
-        }
-    )) 
+        passport.use(new FacebookStrategy(
+            {
+                clientID,
+                clientSecret,
+                callbackURL            
+            },
+            (accessToken, refreshToken, profile, verifyCallback) => {
+                const user = profile;
+                user.token = 'TEST TOKEN';
+                verifyCallback(null, user);
+            }
+        )) 
+    }
+    
 
 }
