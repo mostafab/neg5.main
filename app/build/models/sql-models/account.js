bf7dc5e92854af73d6d523ad32d25cd4f79c75f1
mostafa <mbhuiyan8@gatech.edu>
2016-12-22 10:30:40 -0500

    style changes

--- app/build/models/sql-models/account.js
+++ app/build/models/sql-models/account.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _account = require('../../data-access/account');
@@ -12,57 +12,56 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 exports.default = {
 
-    create: function create(_ref) {
-        var username = _ref.username;
-        var password = _ref.password;
-        var _ref$email = _ref.email;
-        var email = _ref$email === undefined ? null : _ref$email;
-        var _ref$name = _ref.name;
-        var name = _ref$name === undefined ? null : _ref$name;
+  create: function create(_ref) {
+    var username = _ref.username;
+    var password = _ref.password;
+    var _ref$email = _ref.email;
+    var email = _ref$email === undefined ? null : _ref$email;
+    var _ref$name = _ref.name;
+    var name = _ref$name === undefined ? null : _ref$name;
+    return new Promise(function (resolve, reject) {
+      var newAccount = {
+        username: username.trim().toLowerCase(),
+        password: password.trim(),
+        email: !email || email.trim() === '' ? null : email.trim().toLowerCase(),
+        name: !name || name.trim() === '' ? null : name.trim()
+      };
+      _account2.default.saveAccount(newAccount).then(function (user) {
+        return resolve(user);
+      }).catch(function (error) {
+        return reject(error);
+      });
+    });
+  },
 
-        return new Promise(function (resolve, reject) {
-            var newAccount = {
-                username: username.trim().toLowerCase(),
-                password: password.trim(),
-                email: !email || email.trim() === '' ? null : email.trim().toLowerCase(),
-                name: !name || name.trim() === '' ? null : name.trim()
-            };
-            _account2.default.saveAccount(newAccount).then(function (user) {
-                return resolve(user);
-            }).catch(function (error) {
-                return reject(error);
-            });
-        });
-    },
+  findOne: function findOne(_ref2) {
+    var username = _ref2.username;
+    var password = _ref2.password;
+    return new Promise(function (resolve, reject) {
+      var accountToRetrieve = {
+        user: username.trim().toLowerCase(),
+        password: password.trim()
+      };
+      _account2.default.authenticateAccount(accountToRetrieve).then(function (jwt) {
+        return resolve(jwt);
+      }).catch(function (error) {
+        return reject(error);
+      });
+    });
+  },
 
-    findOne: function findOne(_ref2) {
-        var username = _ref2.username;
-        var password = _ref2.password;
-
-        return new Promise(function (resolve, reject) {
-            var accountToRetrieve = {
-                user: username.trim().toLowerCase(),
-                password: password.trim()
-            };
-            _account2.default.authenticateAccount(accountToRetrieve).then(function (jwt) {
-                return resolve(jwt);
-            }).catch(function (error) {
-                return reject(error);
-            });
-        });
-    },
-
-    findByQuery: function findByQuery(query) {
-        return new Promise(function (resolve, reject) {
-            if (!query) return reject(new Error('No query provided'));
-
-            var searchQuery = query.trim().toLowerCase();
-            _account2.default.findByQuery(searchQuery).then(function (users) {
-                return resolve(users);
-            }).catch(function (error) {
-                return reject(error);
-            });
-        });
-    }
+  findByQuery: function findByQuery(query) {
+    return Promise(function (resolve, reject) {
+      if (!query) {
+        reject(new Error('No query provided'));
+      }
+      var searchQuery = query.trim().toLowerCase();
+      _account2.default.findByQuery(searchQuery).then(function (users) {
+        return resolve(users);
+      }).catch(function (error) {
+        return reject(error);
+      });
+    });
+  }
 
 };
--- app/src/models/sql-models/account.js
+++ app/src/models/sql-models/account.js
@@ -1,42 +1,37 @@
 import db from '../../data-access/account';
 
 export default {
-    
-    create: ({username, password, email =  null, name = null}) => {
-        return new Promise((resolve, reject) => {
-            let newAccount = {
-                username: username.trim().toLowerCase(),
-                password: password.trim(),
-                email: (!email || email.trim() === '') ? null : email.trim().toLowerCase(),
-                name: (!name || name.trim() === '') ? null : name.trim()
-            };
-            db.saveAccount(newAccount)
-                .then(user => resolve(user))
-                .catch(error => reject(error));
-        }); 
-    },
-    
-    findOne: ({username, password}) => {
-        return new Promise((resolve, reject) => {
-           let accountToRetrieve = {
-               user: username.trim().toLowerCase(),
-               password: password.trim()
-           };
-           db.authenticateAccount(accountToRetrieve)
-                .then(jwt => resolve(jwt))
-                .catch(error => reject(error)); 
-        });
-    },
 
-    findByQuery: (query) => {
-        return new Promise((resolve, reject) => {
-            if (!query) return reject(new Error('No query provided'));
+  create: ({ username, password, email = null, name = null }) => new Promise((resolve, reject) => {
+    const newAccount = {
+      username: username.trim().toLowerCase(),
+      password: password.trim(),
+      email: (!email || email.trim() === '') ? null : email.trim().toLowerCase(),
+      name: (!name || name.trim() === '') ? null : name.trim(),
+    };
+    db.saveAccount(newAccount)
+        .then(user => resolve(user))
+        .catch(error => reject(error));
+  }),
 
-            let searchQuery = query.trim().toLowerCase();
-            db.findByQuery(searchQuery)
-                .then(users => resolve(users))
-                .catch(error => reject(error));
-        })
+  findOne: ({ username, password }) => new Promise((resolve, reject) => {
+    const accountToRetrieve = {
+      user: username.trim().toLowerCase(),
+      password: password.trim(),
+    };
+    db.authenticateAccount(accountToRetrieve)
+        .then(jwt => resolve(jwt))
+        .catch(error => reject(error));
+  }),
+
+  findByQuery: query => Promise((resolve, reject) => {
+    if (!query) {
+      reject(new Error('No query provided'));
     }
-    
-}
+    const searchQuery = query.trim().toLowerCase();
+    db.findByQuery(searchQuery)
+        .then(users => resolve(users))
+        .catch(error => reject(error));
+  }),
+
+};
