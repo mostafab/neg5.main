f54e027aceb7d1809b9ab24f63d96df66833052d
mostafab <mostafa0104@gmail.com>
2017-06-06 00:46:37 -0400

    more work on scoresheet module port

--- app/public/javascript/modules/controllers/scoresheet-cycle.controller.js
+++ app/public/javascript/modules/controllers/scoresheet-cycle.controller.js
@@ -1,11 +1,13 @@
 export default class ScoresheetCycleController {
-  constructor($scope, ScoresheetService, TeamService) {
+  constructor($scope, ScoresheetService, TeamService, TournamentService) {
     this.$scope = $scope;
     this.ScoresheetService = ScoresheetService;
     this.TeamService = TeamService;
+    this.TournamentService = TournamentService;
     this.game = this.ScoresheetService.game;
     this.teams = this.TeamService.teams;
-
+    this.pointScheme = this.TournamentService.pointScheme;
+    this.rules = this.TournamentService.rules;
     this.$scope.tournamentId = this.$scope.$parent.tournamentId;
   }
 
@@ -21,6 +23,64 @@ export default class ScoresheetCycleController {
 
       });
   }
+
+  getTeamScoreUpToCycle(teamId, cycleIndex) {
+    let score = 0;
+    for (let i = 0; i <= cycleIndex; i++) {
+      const cycle = this.game.cycles[i];
+      cycle.answers.forEach((a) => {
+        if (a.teamId === teamId) {
+          score += a.value;
+        }
+      });
+      score += cycle.bonuses.filter(b => b === teamId).length * this.pointScheme.bonusPointValue;
+    }
+    if (cycleIndex + 1 === this.game.currentCycle.number) {
+      this.game.currentCycle.answers.forEach((a) => {
+        if (a.teamId === teamId) {
+          score += a.value;
+        }
+      });
+      score += this.game.currentCycle.bonuses.filter(b =>
+        b === teamId).length * this.pointScheme.bonusPointValue;
+    }
+    return score;
+  }
+
+  teamDidNotNegInCycle(teamId, cycle) {
+    return cycle.answers.findIndex(answer => answer.type === 'Neg' && answer.teamId === teamId)
+      === -1;
+  }
+
+  removeTeamNegsFromCycle(teamId, cycle) {
+    cycle.answers = cycle.answers.filter(a => !(a.type === 'Neg' && a.teamId === teamId));
+  }
+
+  addPlayerAnswerToCurrentCycle(player, teamInfo, answer) {
+    if (this.teamDidNotAnswerInCycle(teamInfo, this.game.currentCycle)) {
+      this.game.currentCycle.answers.push({
+        playerId: player.id,
+        teamId: teamInfo.id,
+        value: answer.value,
+        type: answer.type,
+      });
+    }
+  }
+
+  teamDidNotAnswerInCycle(team, cycle) {
+    return cycle.answers.findIndex(answer => answer.teamId === team.id) === -1;
+  }
+
+  switchToBonusIfTossupGotten(answer, teamId) {
+    if (answer.type !== 'Neg' && this.teamDidNotNegInCycle(teamId, this.game.currentCycle)) {
+      this.switchCurrentCycleContext(true);
+    }
+  }
+
+  switchCurrentCycleContext(toBonus) {
+    this.game.onTossup = !toBonus;
+  }
+
 }
 
-ScoresheetCycleController.$inject = ['$scope', 'ScoresheetService', 'TeamService'];
+ScoresheetCycleController.$inject = ['$scope', 'ScoresheetService', 'TeamService', 'TournamentService'];
--- app/public/javascript/modules/controllers/scoresheet-form.controller.js
+++ app/public/javascript/modules/controllers/scoresheet-form.controller.js
@@ -1,9 +1,29 @@
 export default class ScoresheetFormController {
-  constructor($scope, ScoresheetService) {
+  constructor($scope, ScoresheetService, TournamentService, TeamService) {
     this.$scope = $scope;
     this.ScoresheetService = ScoresheetService;
+    this.TournamentService = TournamentService;
+    this.TeamService = TeamService;
     this.game = this.ScoresheetService.game;
+    this.pointScheme = this.TournamentService.pointScheme;
+    this.rules = this.TournamentService.rules;
+  }
+
+  addPlayer(team) {
+    const { length } = team.newPlayer;
+    if (length > 0) {
+      this.TeamService.addPlayer(this.$scope.tournamentId, team.teamInfo.id, team.newPlayer)
+        .then((player) => {
+          team.players.push({
+            name: player.name,
+            id: player.id,
+            tuh: 0,
+            active: team.players.length + 1 <= this.rules.maxActive,
+          });
+          team.newPlayer = '';
+        });
+    }
   }
 }
 
-ScoresheetFormController.$inject = ['$scope', 'ScoresheetService'];
+ScoresheetFormController.$inject = ['$scope', 'ScoresheetService', 'TournamentService', 'TeamService'];
