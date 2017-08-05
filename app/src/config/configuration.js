1b5a7df4efc5d2dc52ee2430b31113f888913083
mostafab <mostafa0104@gmail.com>
2017-08-04 21:24:52 -0700

    css changes staaaaart

--- app/src/config/configuration.js
+++ app/src/config/configuration.js
@@ -1,33 +1,6 @@
-import fs from 'fs';
-import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-// const configurationFilePath = path.join(__dirname, '../../../../configuration.json');
-
-// let buffer;
-// let configurationJson;
-
-// try {
-//   buffer = fs.readFileSync(configurationFilePath);
-//   configurationJson = JSON.parse(buffer);
-// } catch (err) { // Grab from environment variables instead
-//   configurationJson = {
-//     databaseConnections: {
-//       postgres: {
-//         PROD: process.env.DB_URL,
-//       },
-//     },
-//     env: process.env.ENV,
-//     port: process.env.PORT,
-//     jwt: {
-//       secret: process.env.JWT_SECRET,
-//     },
-//   };
-// }
-
-// export default configurationJson;
 
 export default {
   ...process.env,
