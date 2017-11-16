3a118624330b1264456095c783aa7f6586d98b95
mostafab <mostafa0104@gmail.com>
2017-11-15 20:00:59 -0800

    Remove unnnessary queries on stats generation

--- app/src/data-access/stats.js
+++ app/src/data-access/stats.js
@@ -1,4 +1,4 @@
-import { readOnlyTransaction, txMap as tx } from '../database/db';
+import { readOnlyQuery, queryTypeMap as qm } from '../database/db';
 import sql from '../database/sql';
 
 const statSQL = sql.statistics;
@@ -8,36 +8,14 @@ const divisionSQL = sql.division;
 
 function formatStatisticsResults(result) {
   return {
-    phase: result[0][0] || { name: 'All Phases', id: null },
-    pointScheme: result[2].tossup_point_scheme.sort((first, second) => second.value - first.value),
-    tournamentName: result[2].name,
-    activePhaseId: result[2].active_phase_id,
-    stats: result[1],
+    stats: result,
   };
 }
 
 export default {
 
   individualReport: (tournamentId, phaseId = null) => new Promise((resolve, reject) => {
-    const queriesArray = [];
-    queriesArray.push(
-      {
-        text: phaseSQL.findById,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: statSQL.individual,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: tournamentSQL.findById,
-        params: [tournamentId],
-        queryType: tx.one,
-      },
-    );
-    readOnlyTransaction(queriesArray)
+    readOnlyQuery(statSQL.individual, [tournamentId, phaseId], qm.any)
         .then((result) => {
           const formattedResult = formatStatisticsResults(result);
           resolve(formattedResult);
@@ -46,112 +24,32 @@ export default {
   }),
 
   teamReport: (tournamentId, phaseId = null) => new Promise((resolve, reject) => {
-    const queriesArray = [];
-    queriesArray.push(
-      {
-        text: phaseSQL.findById,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: phaseId ? statSQL.teamGivenPhase : statSQL.teamDefaultPhase,
-        params: phaseId ? [tournamentId, phaseId] : [tournamentId],
-        queryType: tx.any,
-      },
-      {
-        text: tournamentSQL.findById,
-        params: [tournamentId],
-        queryType: tx.one,
-      },
-      {
-        text: divisionSQL.findByTournament,
-        params: [tournamentId],
-        queryType: tx.any,
-      },
-    );
-    readOnlyTransaction(queriesArray)
+    const query = phaseId ? statSQL.teamGivenPhase : statSQL.teamDefaultPhase;
+    const params = phaseId ? [tournamentId, phaseId] : [tournamentId];
+    readOnlyQuery(query, params, qm.any)
       .then((result) => {
         const formattedResult = formatStatisticsResults(result);
-        if (phaseId) {
-          formattedResult.divisions = result[3].filter(d =>
-            d.phase_id === result[0][0].id);
-        } else {
-          formattedResult.divisions = result[3].filter(d =>
-            d.phase_id === result[2].active_phase_id);
-        }
         resolve(formattedResult);
       })
       .catch(error => reject(error));
   }),
 
   teamFullReport: (tournamentId, phaseId = null) => new Promise((resolve, reject) => {
-    const queriesArray = [];
-    queriesArray.push(
-      {
-        text: phaseSQL.findById,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: statSQL.teamFull,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: tournamentSQL.findById,
-        params: [tournamentId],
-        queryType: tx.one,
-      },
-    );
-    readOnlyTransaction(queriesArray)
+    const query = statSQL.teamFull;
+    const params = [tournamentId, phaseId];
+    readOnlyQuery(query, params, qm.any)
         .then(result => resolve(formatStatisticsResults(result)))
         .catch(error => reject(error));
   }),
 
   playerFullReport: (tournamentId, phaseId = null) => new Promise((resolve, reject) => {
-    const queriesArray = [];
-    queriesArray.push(
-      {
-        text: phaseSQL.findById,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: statSQL.playerFull,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: tournamentSQL.findById,
-        params: [tournamentId],
-        queryType: tx.one,
-      },
-    );
-    readOnlyTransaction(queriesArray)
+    readOnlyQuery(statSQL.playerFull, [tournamentId, phaseId], qm.any)
         .then(result => resolve(formatStatisticsResults(result)))
         .catch(error => reject(error));
   }),
 
   roundReport: (tournamentId, phaseId = null) => new Promise((resolve, reject) => {
-    const queriesArray = [];
-    queriesArray.push(
-      {
-        text: phaseSQL.findById,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: statSQL.roundReport,
-        params: [tournamentId, phaseId],
-        queryType: tx.any,
-      },
-      {
-        text: tournamentSQL.findById,
-        params: [tournamentId],
-        queryType: tx.one,
-      },
-    );
-    readOnlyTransaction(queriesArray)
+    readOnlyQuery(statSQL.roundReport, [tournamentId, phaseId], qm.any)
         .then(result => resolve(formatStatisticsResults(result)))
         .catch(error => reject(error));
   }),
