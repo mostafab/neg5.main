db262870262c9dc94dd34935711cca767be344cf
Mostafa Bhuiyan <mostafa.bhuiyan@redfin.com>
2018-11-24 14:57:47 -0500

    Post to backend on drop team

--- app/public/javascript/modules/controllers/pools.controller.js
+++ app/public/javascript/modules/controllers/pools.controller.js
@@ -1,16 +1,22 @@
 export default class PoolsController {
 
   static get $inject() {
-    return ['DivisionService', 'TeamService', 'PhaseService'];
+    return ['$scope', 'DivisionService', 'TeamService', 'PhaseService'];
   }
 
-  constructor(DivisionService, TeamService, PhaseService) {
+  constructor($scope, DivisionService, TeamService, PhaseService) {
     this.DivisionService = DivisionService;
     this.TeamService = TeamService;
     this.PhaseService = PhaseService;
+
+    this.tournamentId = $scope.tournamentId;
   }
 
-  onDropTeam(team, division) {
-    console.log(division);
+  onDropTeam(team, phase, division) {
+    const divisionMap = {
+      ...team.divisions,
+    };
+    divisionMap[phase.id] = division ? division.id : null;
+    this.TeamService.updateTeamDivisions(this.tournamentId, team.id, divisionMap, Object.values(divisionMap))
   }
 };
